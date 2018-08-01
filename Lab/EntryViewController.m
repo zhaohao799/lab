ab408a19f70cd31538c4f6ab8cbf711e76dfa0ed
Everest_Z <zhaohao799@gmail.com>
2018-08-01 20:34:19 +0800

    alertController 实验

--- Lab/EntryViewController.m
+++ Lab/EntryViewController.m
@@ -33,7 +33,7 @@
     [super viewDidLoad];
     
     self.title = ENTRY_TITLE;
-    self.dataArray = @[@"share test", @"invocation"];
+    self.dataArray = @[@"share test", @"invocation", @"Alert"];
     
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, 100, 100) style:UITableViewStylePlain];
     self.tableView.dataSource = self;
@@ -70,6 +70,53 @@
     if (indexPath.row == 1) {
         InvocationViewController *vc = [[InvocationViewController alloc] init];
         [self.navigationController pushViewController:vc animated:YES];
+    }
+    
+    if (indexPath.row == 2) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"The Alert Action Sheet Title" message:@"The Alert Action Sheet Message" preferredStyle:UIAlertControllerStyleActionSheet];
+        
+        UIAlertAction *firstAction = [UIAlertAction actionWithTitle:@"First Action Title" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"The Alert Title" message:@"The Alert Message" preferredStyle:UIAlertControllerStyleAlert];
+            
+            UIAlertAction *firstAction = [UIAlertAction actionWithTitle:@"first" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                NSLog(@"first");
+            }];
+            [alert addAction:firstAction];
+            
+            UIAlertAction *secondAction = [UIAlertAction actionWithTitle:@"second" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+                NSLog(@"destructive");
+            }];
+            [alert addAction:secondAction];
+            
+            UIAlertAction *thirdAction = [UIAlertAction actionWithTitle:@"third" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                NSLog(@"cancel");
+            }];
+            [alert addAction:thirdAction];
+            
+            [self presentViewController:alert animated:YES completion:nil];
+        }];
+        [alert addAction:firstAction];
+        
+        UIAlertAction *secondAction = [UIAlertAction actionWithTitle:@"Second Action Title" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"second action handler");
+        }];
+        [alert addAction:secondAction];
+        
+        UIAlertAction *thirdAction = [UIAlertAction actionWithTitle:@"cancel action title" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"cancel action handler");
+            
+        }];
+        [alert addAction:thirdAction];
+        
+        UIAlertAction *forthAction = [UIAlertAction actionWithTitle:@"destructive action title" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+            NSLog(@"destructive action handler");
+        }];
+        [forthAction setValue:[UIColor yellowColor] forKey:@"titleTextColor"];
+        [alert addAction:forthAction];
+        
+        [self presentViewController:alert animated:YES completion:^{
+            NSLog(@"present action sheet complete");
+        }];
     }
 }
 
